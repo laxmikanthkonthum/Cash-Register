cc0c2422a8e442d24e3859bed9e13128a3d46d51
laxmikanthkonthum <laxmikanthkonthum@gmail.com>
2021-09-14 18:02:18 +0530

    Added Some Features

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ var billButton = document.querySelector(".bill-button")
 var cashButton = document.querySelector(".cash-button")
 var msg = document.querySelector("#comment")
 var noOfNotes = document.querySelectorAll(".no-of-notes")
-let bill = billBox.value;
-let cash = cashBox.value;
+var resetbtn = document.querySelector(".reset-button")
+var hiddenPart = document.querySelector(".hidden")
+hiddenPart.display = "none"
+// let bill = billBox.value;
+// let cash = cashBox.value;
+billButton.addEventListener('click',function unhide(){
+  hiddenPart.display = "block"
+})
+var captionVal = document.querySelector(".caption")
+resetbtn.addEventListener('click',function refresh(){
+  location.reload();
+})
 //console.log('Bill Outside - ' , bill)
 Notes = ['2000','500','200','50','20','10'];
 cashButton.addEventListener('click',function Logic(){
     hidemessage();
     bill = Number(billBox.value)
     cash = Number(cashBox.value)
-    console.log(typeof(bill))
-    console.log(typeof(cash))
-    console.log(bill)
-    console.log(cash)
+    // console.log(typeof(bill))
+    // console.log(typeof(cash))
+    // console.log(bill)
+    // console.log(cash)
 
   if(bill > cash){
     message = "Error: Cash amount should be greater than or equal to Bill Amount";
@@ -23,6 +33,9 @@ cashButton.addEventListener('click',function Logic(){
   }  
   else{
     var returnAmt = cash - bill;
+
+    captionVal.innerText = "Amount to be returned is: " + returnAmt
+
     if(returnAmt == 0){
       message = "We are good! :)"
       display(message);
@@ -30,9 +43,11 @@ cashButton.addEventListener('click',function Logic(){
     for(var i = 0; i<Notes.length; i++){
       var val = Math.trunc(returnAmt/Notes[i]);
       returnAmt = returnAmt % Notes[i]; 
-      console.log("val: " + val + " returnAmt: " + returnAmt )
+      //console.log("val: " + val + " returnAmt: " + returnAmt )
+      if(val != 0){
       noOfNotes[i].innerText =  val
     }
+  }
       
   }
 });
